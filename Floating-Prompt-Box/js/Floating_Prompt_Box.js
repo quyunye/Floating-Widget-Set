573079b010e38f24518f2fc418baf547dc0535be
GYqyy <G@GycTu.com>
2019-09-11 17:08:56 +0800

    Add files via upload

---
+++ Floating-Prompt-Box/js/Floating_Prompt_Box.js
@@ -0,0 +1,16 @@
+/*
+* Copyright (c) 2015 GYqyy All rights reserved.
+* Url：http://www.gyqyy.com/
+*/
+function show(tag){
+ var light=document.getElementById(tag);
+ var fade=document.getElementById('fade');
+ light.style.display='block';
+ fade.style.display='block';
+ }
+function hide(tag){
+ var light=document.getElementById(tag);
+ var fade=document.getElementById('fade');
+ light.style.display='none';
+ fade.style.display='none';
+}
